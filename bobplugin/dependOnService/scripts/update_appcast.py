720711596f56ab0676c4a4616d81ecb0194a08bc
ZhangDing <akl7777777@163.com>
2023-03-05 18:55:07 +0800

    修复自动化脚本

--- bobplugin/dependOnService/scripts/update_appcast.py
+++ bobplugin/dependOnService/scripts/update_appcast.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 def update_appcast(message):
-    with open ("src/info.json", "r") as f:
+    with open ("bobplugin/dependOnService/src/info.json", "r") as f:
         info = json.load(f)
     version = info["version"]
     release_file = Path(f"release/bob-plugin-akl-youdao-free-translate.bobplugin")
@@ -20,7 +20,7 @@ def update_appcast(message):
         "url": f"https://github.com/akl7777777/bob-plugin-akl-youdao-free-translate/releases/download/v{version}/bob-plugin-akl-youdao-free-translate_v{version}.bobplugin",
         "minBobVersion": "0.5.0"
     }
-    appcast_file = Path("appcast.json")
+    appcast_file = Path("bobplugin/dependOnService/appcast.json")
     if appcast_file.is_file():
         with open(appcast_file, "r") as f:
             appcast = json.load(f)
